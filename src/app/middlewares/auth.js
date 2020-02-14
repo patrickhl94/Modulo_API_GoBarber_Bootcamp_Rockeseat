cfc0159e3e03fab967a55a685aa4512db50b6d5a
Patrick Lima <patrickhl94@hotmail.com>
2020-02-13 23:03:20 -0300

    Create autentication JWT

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const autHeader = req.headers.authorization;
+
+  if (!autHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = autHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    console.log(decoded);
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ erro: 'Token invalid' });
+  }
+};

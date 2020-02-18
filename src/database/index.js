8c6d81458b73a7f257ec63566e5ce3083636ec91
Patrick Lima <patrickhl94@hotmail.com>
2020-02-18 00:05:46 -0300

    Insert relationship between tables

--- src/database/index.js
+++ src/database/index.js
@@ -16,6 +16,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 

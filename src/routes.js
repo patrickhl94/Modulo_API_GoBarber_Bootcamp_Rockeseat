3de87186df64d03d3bf7c1668be6aa3ac7b06474
Patrick Lima <patrickhl94@hotmail.com>
2020-02-19 17:31:09 -0300

    Create router post appointments

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,8 +18,13 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/appointments', AppointmentsController.store);
+
 routes.get('/providers', ProviderController.index);
+
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

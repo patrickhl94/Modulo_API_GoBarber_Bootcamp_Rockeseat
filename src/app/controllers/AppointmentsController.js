543d413282a7975971214496ae91e2da8faa06aa
Patrick Lima <patrickhl94@hotmail.com>
2020-02-19 18:16:08 -0300

    Update Appointments Controller

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -1,6 +1,39 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import Appointment from '../models/Appointments';
+
 class AppointmentController {
   async store(req, res) {
-    return res.json({});
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+    /**
+     * Check if provider_id is a provider
+     */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointment whith providers' });
+    }
+
+    const appointment = await Appointment.create({
+      users_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
   }
 }
 

b9df7e068bc46141b184ecea920b0a0eaaa7a5f4
Patrick Lima <patrickhl94@hotmail.com>
2020-03-10 17:08:21 -0300

    Add log to failed Queue

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -30,8 +30,12 @@ class Queue {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
 
-      bee.process(handle);
+      bee.on('failed', this.handleFailure).process(handle);
     });
+  }
+
+  handleFailure(job, erro) {
+    console.log(`Queue ${job.queue.name}: FAILED`, erro);
   }
 }
 

a6fc247bdb8b6698a130b6a247b444794519fb03
Patrick Lima <patrickhl94@hotmail.com>
2020-03-11 22:31:58 -0300

    Create route AvailableController

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -29,6 +30,7 @@ routes.post('/appointments', AppointmentsController.store);
 routes.delete('/appointments/:id', AppointmentsController.delete);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.put('/users', UserController.update);
 

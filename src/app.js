fcc79df75ac9ebd75a90aff6f43f22f8ca9de6ef
Patrick Lima <patrickhl94@hotmail.com>
2020-03-11 23:44:08 -0300

    Update App

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import path from 'path';
+// Youch dependencia para formatar o erro, o deixando com uma melhor visualização.
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+
+// a importação abaixo deve ficar antes das rotas.
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database';
@@ -7,11 +15,17 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // middleware de verificação de erros.
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +35,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // middleware de tratamento de exceções.
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 export default new App().server;
